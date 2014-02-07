8e6630183c386fe114bd430d2f9b9a0247cca730
pramesh <pramesh@192.168.113.129>
2014-02-07 11:32:07 -0600

    Added UITableView to ViewController

--- IPScanner/ViewController.m
+++ IPScanner/ViewController.m
@@ -13,13 +13,41 @@
 @end
 
 @implementation ViewController
+@synthesize myTableView;
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	arrBarcode = [[NSMutableArray alloc]init];
 }
 
+// UITableView Delegate Methods
+-(NSInteger)numberOfSectionsInTableView: (UITableView *)tableView{
+    return 1;
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    NSLog(@" %i ", arrBarcode.count);
+    return [arrBarcode count];
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    static NSString *CellIdentifier = @"Cell Identifier";
+    [tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:CellIdentifier];
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+    NSString *barcode = [arrBarcode objectAtIndex:[indexPath row]];
+    // Create custom color
+    UIColor *color = [UIColor colorWithRed:0/255.0f green:150/255.0f blue:225/255.0f alpha:1.0f];
+    cell.textLabel.textColor = color;
+    cell.textLabel.font = [UIFont fontWithName:@"Helvetica" size:14.0];    [cell.textLabel setText:barcode];
+    [cell setBackgroundColor:[UIColor clearColor]];
+    //UIButton *btn = [[UIButton alloc]init];
+    //btn.frame = CGRectMake(200, 5.0, 50, 30);
+    //btn.backgroundColor = [UIColor blackColor];
+    //[btn setTitle:@"Send" forState:UIControlStateNormal];
+    //[cell addSubview:btn];
+    return cell;
+}
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
